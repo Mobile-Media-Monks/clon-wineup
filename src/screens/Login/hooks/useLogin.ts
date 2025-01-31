0f5eea38937f09b2680472cc365976240f31f4e3
jorge.viveros <jorge.viveros@mediamonks.com>
2025-01-31 10:17:52 -0500

    refactor: remove theme color

--- src/screens/Login/hooks/useLogin.ts
+++ src/screens/Login/hooks/useLogin.ts
@@ -1,23 +1,9 @@
-import { useMemo, useCallback } from 'react';
-import { DISABLED } from '@/components/FormInput/constants';
-import { Theme } from '@/theme/ThemeProvider/types';
-import { addAlpha } from '@/utils/commons';
-import { useThemeContext } from '@/theme/ThemeProvider';
+import { useCallback } from 'react';
 import { LoginData } from '../types';
 import { useAuth } from '@/hooks/useAuth';
 
 export const useLogin = () => {
   const { login, loading: isHandlingLogin } = useAuth();
-  const { theme: themeContext } = useThemeContext();
-  const colors = themeContext?.colors;
-
-  const loginMethodNameColor = useMemo(
-    () =>
-      isHandlingLogin
-        ? addAlpha(colors.theme[Theme.WINE].four, DISABLED)
-        : colors.theme[Theme.WINE].four,
-    [isHandlingLogin],
-  );
 
   const handleLoginWithEmailAndPassword = useCallback((data: LoginData) => {
     login(data.email, data.password);
@@ -25,7 +11,6 @@ export const useLogin = () => {
 
   return {
     isHandlingLogin,
-    loginMethodNameColor,
     handleLoginWithEmailAndPassword,
   };
 };
